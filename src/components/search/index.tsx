514a8bc55b47fa1c51b223324b000bfadf39e426
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-11 14:46:06 -0300

    feat: Search Component

---
+++ src/components/search/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./index.modules.scss"
+import Image from "next/image"
+
+export default function Search(){
+    return(
+        <form className={styles.form}action="">
+            <input type="text" placeholder="Encuentra tu comic favorito..." className={styles.input}/>
+            <button type="submit" className={styles.button}>
+                <Image src="/search.svg" alt="search icon" width={35} height={35}/>
+            </button>
+        </form>
+    )
+}
