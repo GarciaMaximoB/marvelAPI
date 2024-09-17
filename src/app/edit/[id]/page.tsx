4a8a4817dda88c9ac4c68949818085783cc86b59
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-17 14:40:21 -0300

    feat: edit page

---
+++ src/app/edit/[id]/page.tsx
@@ -0,0 +1,13 @@
+"use client";
+import styles from "./index.module.scss";
+
+import Formulario from "@/components/form";
+
+export default function Edit({ params }: { params: { id: string } }) {
+  return (
+    <div className={styles.container}>
+      <h1>Editando el comic {params.id}</h1>
+      <Formulario edit />
+    </div>
+  );
+}
