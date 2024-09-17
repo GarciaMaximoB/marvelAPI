ca57b1c5089c40fc7b665ee592e8aba0c4fd5046
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-17 14:37:10 -0300

    feat: homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,39 @@
 import Link from "next/link";
-import styles from "./index.module.scss"
+import styles from "./index.module.scss";
 import Search from "@/components/search";
 import Card from "@/components/card";
 
+import { StarOutlined, PlusOutlined } from "@ant-design/icons";
+import Filters from "@/components/Filters";
 export default function Home() {
   return (
     <div className={styles.header}>
-        <Link href="/favourites">FAVORITOS</Link>
-        <Search/>
-        <div className={styles.cardsWrapper}>
-          <Card/><Card/><Card/><Card/><Card/><Card/><Card/><Card/>
+      <Search />
+      <div className={styles.buttonsContainer}>
+        <div className={styles.buttons}>
+          <Link href="/create" className={styles.createButton}>
+            <PlusOutlined />
+            CREAR COMIC
+          </Link>
+          <Link href="/favourites" className={styles.favButton}>
+            <StarOutlined />
+            FAVORITOS
+          </Link>
         </div>
+        <div className={styles.filters}>
+          <Filters />
+        </div>
+      </div>
+      <div className={styles.cardsWrapper}>
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
     </div>
   );
 }
