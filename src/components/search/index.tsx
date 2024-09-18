84992020fc0b823cbee8c719b39fe9237afe6651
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-18 16:05:10 -0300

    prettier format

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,13 +1,17 @@
-import styles from "./index.module.scss"
-import Image from "next/image"
+import styles from "./index.module.scss";
+import Image from "next/image";
 
-export default function Search(){
-    return(
-        <form className={styles.form}action="">
-            <input type="text" placeholder="Encuentra tu comic favorito..." className={styles.input}/>
-            <button type="submit" className={styles.button}>
-                <Image src="/search.svg" alt="search icon" width={35} height={35}/>
-            </button>
-        </form>
-    )
+export default function Search() {
+  return (
+    <form className={styles.form} action="">
+      <input
+        type="text"
+        placeholder="Encuentra tu comic favorito..."
+        className={styles.input}
+      />
+      <button type="submit" className={styles.button}>
+        <Image src="/search.svg" alt="search icon" width={35} height={35} />
+      </button>
+    </form>
+  );
 }
