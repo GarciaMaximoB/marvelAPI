3917f05ed6b9036da2236ee022101ddf96637736
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-18 16:04:32 -0300

    feat: agregar comics a favoritos

--- src/components/card/index.tsx
+++ src/components/card/index.tsx
@@ -1,12 +1,43 @@
-import styles from "./index.module.scss"
+"use client";
+import styles from "./index.module.scss";
+import { StarOutlined, StarFilled } from "@ant-design/icons";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
 
-export default function Card(){
-    return(
-        <div className={styles.mycard}>
-            <img src="/spiderman-comic.jpg" alt="Tapa de comic de spiderman" className={styles.imagePortada}/>
-            <p className={styles.comicTitle}>SPIDER-MAN: HOMEROOM HEROES (2024) #1</p>
-            <p className={styles.comicPages}>32 pages</p>
-            <img src="/star.svg" alt="icono de estrella" className={styles.comicFav}/>
-        </div>
-    )
+export default function Card() {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const router = useRouter();
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  const id = 1;
+
+  return (
+    <div className={styles.mycard}>
+      <div onClick={() => router.push(`/comic/${id}`)}>
+        <img
+          src="/spiderman-comic.jpg"
+          alt="Tapa de comic de spiderman"
+          className={styles.imagePortada}
+        />
+        <p className={styles.comicTitle}>
+          SPIDER-MAN: HOMEROOM HEROES (2024) #1
+        </p>
+        <p className={styles.comicPages}>32 pages</p>
+      </div>
+
+      {isFavorite ? (
+        <StarFilled
+          className={styles.comicFav}
+          onClick={toggleFavorite}
+          style={{ color: "#fcba03" }}
+        />
+      ) : (
+        <StarOutlined className={styles.comicFav} onClick={toggleFavorite} />
+      )}
+    </div>
+  );
 }
