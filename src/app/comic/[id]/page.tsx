e8c98eaab8aac503b990a7d9fb6ade604408416f
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-09-24 14:35:58 -0300

    feat: incluyendo funcionalidad con la API a comicPage

--- src/app/comic/[id]/page.tsx
+++ src/app/comic/[id]/page.tsx
@@ -1,36 +1,44 @@
+"use client";
+import { GlobalStateService } from "@/services/globalStateService";
+import { useEffect, useState } from "react";
+import { ComicsUseCases } from "@/useCases/comicsUseCases";
+
 import styles from "./index.module.scss";
 export default function ComicPage({ params }: { params: { id: string } }) {
+  const [loading, setLoading] = useState(true);
+  const comic = GlobalStateService.getComicData();
+
+  useEffect(() => {
+    const id = params.id;
+    ComicsUseCases.retrieveComic({ id }).finally(() => {
+      setLoading(false);
+    });
+  }, [params.id]);
+  if (loading) return <p>Cargando comic...</p>;
   return (
-    <div className={styles.background}>
+    <div
+      className={styles.background}
+      style={{
+        backgroundImage: `url(${comic.thumbnail.path}.${comic.thumbnail.extension})`,
+      }}
+    >
       <div className={styles.comicContainer}>
         <img
-          src="/spiderman-comic.jpg"
-          alt="Comic de Spider-Man"
+          src={`${comic.thumbnail.path}.${comic.thumbnail.extension}`}
+          alt={comic.title}
           className={styles.comicImage}
         />
         <div className={styles.comicInfo}>
-          <h2 className={styles.comicTitle}>
-            SPIDER-MAN: HOMEROOM HEROES (2024) #{params.id}
-          </h2>
-          <p className={styles.comicDescription}>
-            MARVEL&apos;S NEW COMICS FOR YOUNG READERS CONTINUE! Old foes, new
-            tricks! Teenage super hero Spider-Man has enough on his plate
-            keeping New York City safe from the likes of Electro and Tombstone -
-            but when you add in his life as high-schooler Peter Parker, things
-            get REALLY stressful. Luckily, Spidey&apos;s got web-shooters,
-            Spider-Sense, some super-friends, and a knack for creative
-            problem-solving on his side! Featuring two ten-page short stories,
-            this new series is the perfect introduction to the Marvel Universe
-            and the wall-crawling world of Spider-Man!
-          </p>
+          <h2 className={styles.comicTitle}>{comic.title}</h2>
+          <p className={styles.comicDescription}>{comic.description}</p>
           <div className={styles.comicDetails}>
             <span>
               <p className={styles.comicDetailsTitle}>PUBLICADO:</p>
-              <p>2024-11-27</p>
+              <p>{comic.sale_date}</p>
             </span>
             <span>
               <p className={styles.comicDetailsTitle}>PÁGINAS:</p>
-              <p>32</p>
+              <p>{comic.pageCount}</p>
             </span>
           </div>
         </div>
