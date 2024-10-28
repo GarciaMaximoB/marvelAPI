542c969419d0774028f34a02c1e6c23f19565172
Maximo Bautista Garcia <maximogarciadev@gmail.com>
2024-10-28 15:19:44 -0300

    feat: implementacion de un debounce para hacer la busqueda de comics

--- src/components/search/index.tsx
+++ src/components/search/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import Image from "next/image";
 import styles from "./index.module.scss";
 import { ComicsUseCases } from "@/useCases/comicsUseCases";
 import { GlobalStateService } from "@/services/globalStateService";
+import { debounce } from "@/utils/debounce";
 
 interface SearchProps {
   onSearch: (query: string) => void;
@@ -11,20 +12,31 @@ interface SearchProps {
 export default function Search({ onSearch }: SearchProps) {
   const [query, setQuery] = useState("");
 
-  const handleSearchSubmit = (e: React.FormEvent) => {
-    e.preventDefault();
-    console.log(query)
-    onSearch(query);
+  const debouncedSearch = useCallback(
+    debounce((searchQuery: string) => onSearch(searchQuery), 300),
+    [onSearch]
+  );
+
+  useEffect(() => {
+    if (query === "") {
+      onSearch("");
+    } else {
+      debouncedSearch(query);
+    }
+  }, [query, debouncedSearch, onSearch]);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuery(e.target.value);
   };
 
   return (
-    <form className={styles.form} onSubmit={handleSearchSubmit}>
+    <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
       <input
         type="text"
         placeholder="Encuentra tu comic favorito..."
         className={styles.input}
         value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={handleInputChange}
       />
       <button type="submit" className={styles.button}>
         <Image src="/search.svg" alt="search icon" width={35} height={35} />
